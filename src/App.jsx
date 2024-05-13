239891c71625748e89aa67a297b31d9682578a7a
Sai Sourabh <saisourabhcherukuru@gmail.com>
2024-05-13 23:12:12 +0530

    adding the styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,58 +6,62 @@ import { chunk } from 'lodash';
 import { Col, Row } from 'antd';
 
 function App() {
+    
   const [ productsData, setProductsData ] = useState([]);
-
+  //Fetching the data and dividing the data using chunk
   useEffect(() => {
     async function fetchData() {
       const response = await axios.get("https://dummyjson.com/products");
       console.log({response: response.data.products})
       const chunkedArray = chunk(response.data.products, 3)
       console.log({chunkedArray})
-      setProductsData(response.data.products)
+      setProductsData(chunkedArray)
     }
     fetchData()  
   }, []) 
-
+//Arranging the 3 cards in each row
   const cardsJSX = productsData.map((product) => (
     <div>
       <Row gutter={24}>
         <Col span={8}>
+          {product.length > 0 &&
           <CardComponent
-            brand = {product.brand}
-            description = {product.description}
-            images = {product.images}
-            price = {product.price}
-            discountPercentage = {product.discountPercentage}
-          />
+            brand = {product[0].brand}
+            description = {product[0].description}
+            images = {product[0].images}
+            price = {product[0].price}
+            discountPercentage = {product[0].discountPercentage}
+          />}
         </Col>
         <Col span={8}>
+          {product.length > 1 &&
           <CardComponent
-            brand = {product.brand}
-            description = {product.description}
-            images = {product.images}
-            price = {product.price}
-            discountPercentage = {product.discountPercentage}
-          />
+            brand = {product[1].brand}
+            description = {product[1].description}
+            images = {product[1].images}
+            price = {product[1].price}
+            discountPercentage = {product[1].discountPercentage}
+          />}
         </Col>
         <Col span={8}>
+        {product.length > 2 &&
           <CardComponent
-            brand = {product.brand}
-            description = {product.description}
-            images = {product.images}
-            price = {product.price}
-            discountPercentage = {product.discountPercentage}
-          />
-        </Col>  
+            brand = {product[2].brand}
+            description = {product[2].description}
+            images = {product[2].images}
+            price = {product[2].price}
+            discountPercentage = {product[2].discountPercentage}
+          />}
+        </Col>
     </Row>
   </div>
   ))
 
   return (
-      <div>
-        <h1>
-          Product 
-        </h1>
+     <div>
+      <h1 className='text-xl mx-20'>
+        Products
+      </h1>
         {cardsJSX}
     
       </div>  
--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import { Card, Col, Row } from 'antd';
+import { Card, Col, Row, Button, notification, App, Carousel } from 'antd';
 
+// import { ReactComponent as CartIcon } from '../icons/shopping-cart.svg';
+let click = () => {
+  alert("added to cart");
+}
 
-const CardComponent = (props) => (
-      <Card bordered={true}>
-        <img src={props.images} />
-        <span>{props.description}</span>
-        <span>{props.price}</span>
+const CardComponent = (props) => {
+  props.images.forEach((image) => console.log(image))
+
+  const imagesJSX = props.images.map((image) => (
+    <img src={image} />
+  ))
+
+  return(
+      <Card bordered={true} className="mx-3">
+        <Carousel arrows infinite = {false}>
+          {imagesJSX}
+        </Carousel>
+        <span><b>{props.description}</b></span>
+        <hr/ >
+        <span><b>₹{props.price}   <span class="p-4 text-left text-red-600">{props.discountPercentage}</span>
+      </b></span>
         <br />
-        <button>ADD TO CART</button>
+   
+      
+      <br />
+        <button class =" border-2 border-blue-600 text-blue-600 hover:bg-blue-900" onClick={() => {
+            alert("Added to Cart");
+          }}>
+              
+          <span>🛒 Add to Cart</span>
+
+        </button>
+        
+        {/* <Button icon={CartIcon} iconPosition='start'>ADD TO CART</Button> */}
       </Card>
 //         <Card bordered={true}>
 //         {props.price}
 //         <br />
 //         <button>ADD TO CART</button>
-//         <div class="bg-blue-500 group ...">
-//     <p class="text-blue-300 group-hover:text-white">Click for more</p>
-// </div>
+//         
+//     
+// 
       // </Card>
-);
+)
+};
 
 
 
